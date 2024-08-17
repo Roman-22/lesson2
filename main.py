4dca72b4c9cfbfeb3787c2d52024e8b73a0bddf8
Roman-22 <zaruba195@yandex.ru>
2024-08-17 11:52:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+course = "Piton"
+total_tasks = 12
+hours_spent = 1.5
+
+print ("курс:", course, "всего задач:", total_tasks, "затрачено часов:", hours_spent, "среднее время выполнения:", hours_spent / total_tasks)
+
